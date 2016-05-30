349bc8454ce7f7ca0fffac74872b5ca2679bbc71
j777y <tojerryjoseph@gmail.com>
2016-05-30 15:48:48 +0530

    Update index.php

--- pilots/applandr/index.php
+++ pilots/applandr/index.php
@@ -2,5 +2,5 @@
 	require_once('../../TwitterAutoPilot.php');
 
 	$tap = new TwitterAutoPilot;
-	$tap->init("YOUR_APP_CONSUMER_KEY", "YOUR_APP_CONSUMER_SECRET", "YOUR_APP_ACCESS_TOKEN", "YOUR_APP_ACCESS_SECRET");
+	$tap->init("8Up9awDfUzk3Azq8QCyc0zVSw", "j2JvXqB6jsMU6efFgAHITErUURS855Jadp62xNjF104sEcziyh", "728925276751663106-uRqKRjoKZvvtOct9ndNh2GJ6Y4crQ0j", "Vt5sS5EgedifMOZ2YlhvTcecVuhOB4HQ772jVRikVFefO");
 ?>
